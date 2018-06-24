f7be0763b3db53847704e29589b5279a98b9a0b3
Michael Pope <popemray1986@gmail.com>
2018-06-24 03:47:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const ranks= document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < ranks.length; i++) {
+    let children = ranks[i].children
+
+      for (let j = 0, k = children.length; j < k; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n
+      }
+    }
+  }
+
+function deepestChild() {
+let div = document.getElementById('grand-node')
+let div1 = div.children[0]
+let div2 = div1.children[0]
+let div3 = div2.children[0]
+let div4 = div3.children[0]
+return div4
+
+}

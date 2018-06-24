c80cae61deafd6af5471acbb4afc9c61cb53b930
Michael Pope <popemray1986@gmail.com>
2018-06-24 03:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
  return document.querySelector(selector)
 }
 
-function nestedTarget(target) {
-  return document.querySelector(target)
+function nestedTarget() {
+  return document.querySelector(#nested, .target)
 }

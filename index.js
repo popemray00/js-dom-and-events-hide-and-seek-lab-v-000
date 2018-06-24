751147cbbd49f023d060308fad6b57e166a57c40
Michael Pope <popemray1986@gmail.com>
2018-06-24 03:41:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
+function increaseRankBy(n){
+  const ranks= document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < ranks.length; i++) {
+    let children = ranks[i].children
+
+      for (let j = 0, k = children.length; j < k; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n
+      }
+    }
+  }
+
 function deepestChild() {
   
 }
